d919565025bd7b337c73c2a82fd275a9efe1e69e
Jodie Kurnia <jodiekurnia@outlook.com>
2023-01-26 14:03:47 +0700

    better

--- telegram_gcloner/utils/fire_save_files.py
+++ telegram_gcloner/utils/fire_save_files.py
@@ -60,7 +60,6 @@ class MySaveFileThread(threading.Thread):
                 '-P',
                 '--stats',
                 '1s',
-                '--ignore-size'
             ]
             if config.GCLONE_PARA_OVERRIDE:
                 command_line.extend(config.GCLONE_PARA_OVERRIDE)
